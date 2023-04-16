02e3649e690ef24e00e2a29393ed95ea213f6d5f
Nathan Collins <npcollins@gmail.com>
2023-04-16 18:34:13 -0400

    Branding text updates

---
+++ themes/browser/branding/yikwid/configure.sh
@@ -0,0 +1,12 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# there is a possible patch to consider when changing this:
+# see: patches/browser-confvars.patch
+
+MOZ_APP_NAME=yikwid
+MOZ_APP_BASENAME="Yikwid"
+MOZ_APP_PROFILE=yikwid
+MOZ_APP_VENDOR=Yikwid
+MOZ_APP_DISPLAYNAME=Yikwid
